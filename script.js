cde8a0439a894f618c0fcf0a93699264e24214c6
dudalszz <duclsza@gmail.com>
2025-08-19 18:34:53 -0300

    add page de perfil e editar perfil

---
+++ script.js
@@ -0,0 +1,13 @@
+function irParaEdicao() {
+  window.location.href = "editar.html";
+}
+
+function voltarParaPerfil() {
+  window.location.href = "index.html";
+}
+
+document.getElementById("form-edicao")?.addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Dados salvos com sucesso!");
+  voltarParaPerfil();
+});
